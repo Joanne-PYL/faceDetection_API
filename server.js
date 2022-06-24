159a8268b6657bb01d9ccc3b235c03c47376cf92
Joanne Lee <joanne.pinyulee@gmail.com>
2022-06-24 04:33:48 -0400

    changing response

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(cors());
 
 
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send('it is working');
 })
 
 app.post('/signin', (req, res) => { signin.handleSingin(req, res, db, bcrypt) })
