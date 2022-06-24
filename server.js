d8b4b4d055f26cddf559430da4d5251bae41460b
Joanne Lee <joanne.pinyulee@gmail.com>
2022-06-24 04:30:35 -0400

    adding port

--- server.js
+++ server.js
@@ -44,9 +44,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-// const DATABASE_URL = process.env.DATABASE_URL
-app.listen(3000, () => {
-    console.log('app is running at 3000');
-    // console.log(`app is running at ${DATABASE_URL}`);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running at ${process.env.PORT}`);
 }) 
-
