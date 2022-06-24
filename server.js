4170c8d168edea67537d3011420c85b3397d38fb
Joanne Lee <joanne.pinyulee@gmail.com>
2022-06-24 04:20:12 -0400

    upload

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bcrypt = require('bcrypt-nodejs')
+const knex = require('knex')
+const register = require('./controllers/register');
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+const imageurl = require('./controllers/image')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        //   socketPath : '/path/to/socket.sock',
+        host : '127.0.0.1',
+        user : '',
+        password : '',
+        database : 'smart-brain'
+    }
+  });
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+/* Overview
+/ signin -> POST = success/fail
+/ register -> POST = user
+/ profile/:userId -> GET = user
+/ image -> PUT -> user
+*/
+
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => { signin.handleSingin(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
+// const DATABASE_URL = process.env.DATABASE_URL
+app.listen(3000, () => {
+    console.log('app is running at 3000');
+    // console.log(`app is running at ${DATABASE_URL}`);
+}) 
+
